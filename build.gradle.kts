8304931427d95d5075a15536887fa73638932f17
Dmitry Kovalev <trigger1610@gmail.com>
2026-04-24 19:51:47 +0400

    Initialize Gradle project with Ktor, Exposed, and PostgreSQL dependencies
      Sets up the build system with shadow JAR plugin for fat jar packaging,
      ready for Render deployment. Gradle wrapper copied from sibling KMP project (8.13).

---
+++ build.gradle.kts
@@ -0,0 +1,48 @@
+val ktorVersion = "3.0.3"
+val exposedVersion = "0.57.0"
+val kotlinxDatetimeVersion = "0.6.1"
+val logbackVersion = "1.5.12"
+val hikariVersion = "5.1.0"
+val postgresVersion = "42.7.3"
+
+plugins {
+    kotlin("jvm") version "2.1.0"
+    kotlin("plugin.serialization") version "2.1.0"
+    id("com.github.johnrengelman.shadow") version "8.1.1"
+    application
+}
+
+group = "dmitrykovalev.stringlife"
+version = "0.0.1"
+
+application {
+    mainClass.set("dmitrykovalev.stringlife.ApplicationKt")
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("io.ktor:ktor-server-core:$ktorVersion")
+    implementation("io.ktor:ktor-server-netty:$ktorVersion")
+    implementation("io.ktor:ktor-server-content-negotiation:$ktorVersion")
+    implementation("io.ktor:ktor-serialization-kotlinx-json:$ktorVersion")
+    implementation("io.ktor:ktor-server-status-pages:$ktorVersion")
+
+    implementation("org.jetbrains.exposed:exposed-core:$exposedVersion")
+    implementation("org.jetbrains.exposed:exposed-jdbc:$exposedVersion")
+    implementation("org.jetbrains.exposed:exposed-kotlin-datetime:$exposedVersion")
+    implementation("org.jetbrains.kotlinx:kotlinx-datetime:$kotlinxDatetimeVersion")
+
+    implementation("org.postgresql:postgresql:$postgresVersion")
+    implementation("com.zaxxer:HikariCP:$hikariVersion")
+
+    implementation("ch.qos.logback:logback-classic:$logbackVersion")
+}
+
+tasks.shadowJar {
+    archiveClassifier.set("")
+    archiveVersion.set("")
+    mergeServiceFiles()
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,1 @@
+rootProject.name = "string-life-backend"
